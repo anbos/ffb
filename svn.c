ac8f01a2745085ee68431e73536e195ead2344a7
Your Name <you@example.com>
2009-12-24 17:03:11 +0800

    add a svn

---
+++ svn.c
@@ -0,0 +1,10 @@
+base operation
+1. svn co http://10.31.131.231/repos
+2. modify your text(.c)
+3. svn diff  
+4. svn commit
+5. svn update
+
+..............................................
+
+
--- ter.c
+++ ter.c
@@ -1,6 +1,7 @@
 iern k,e,leo.
-diff -Nur test1 test2
+diff -Nur test1 test2 //get a patch command 
 
 diff -c -b -r test1 test2
 
-
+preference websit
+//http://www.ibm.com/developerworks/cn/linux/l-diffp/index.html
